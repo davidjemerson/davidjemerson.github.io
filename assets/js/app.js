3b667fe773bb1dc128806fb4b0741c9c0037f9de
djemerson <djemerson@gmail.com>
2018-11-27 02:48:40 -0600

    added scrollspy functions, additional projects, style changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@
 var project = [
     {
         title: "EventPlan",
+        subtitle: "easy event scheduling",
         image: "./assets/images/eventplan.png",
         description: "<p>EventPlan will be the best way to get your busy friend group together for game night, dinner, or just a few beers.</p>" +
                         "<p>This is a MERN stack application initially coded over the course of two weeks in between other work. I mainly focused on the database, models, authoring the server, and user authentication.</p>" +
@@ -11,6 +12,7 @@ var project = [
     },
     {
         title: "Where's the Show",
+        subtitle: "quick concert info",
         image: "./assets/images/wts.png",
         description: "<p>Where's The Show was born from the idea that it should be easier to quickly find local music events in your area, with a fast and clean interface.</p>" +
                         "<p>The front-end was built using Materialize components, with simple AJAX API calls and jQuery manipulation.</p>",
@@ -19,6 +21,7 @@ var project = [
     },
     {
         title: "Doctor Who Memory",
+        subtitle: "react based fun",
         image: "./assets/images/doctorwho.png",
         description: "<p>First things first - I love Doctor Who</p>" +
                         "<p>This app was the first thing that came out of my work with React.</p>" +
@@ -27,7 +30,53 @@ var project = [
         live: "https://davidjemerson.github.io/memory/"
     },
     {
+        title: "Project Three-Sixty",
+        subtitle: "project cost tracking",
+        image: "./assets/images/pts.png",
+        description: "<p>Project Three-Sixty is a web app built to help track project costs for one of the co-developer's painting business.</p>" +
+                        "Technologies used include Express, Sequelize, MySQL, and Passport.",
+        github: "https://github.com/davidjemerson/project-threesixty",
+        live: "https://project-three-sixty.herokuapp.com"
+    },
+    {
+        title: "Liri",
+        subtitle: "command line fun with node",
+        image: "./assets/images/liri.png",
+        description: "<p>A node based command line application that performs a few basic functions, including downloading my most recent Twitter blatherings.</p>",
+        github: "https://github.com/davidjemerson/liri-node-app",
+        live: "https://github.com/davidjemerson/liri-node-app"
+    },
+    {
+        title: "Game Gif Getter",
+        subtitle: "alliterative API application",
+        image: "./assets/images/gifgetter.png",
+        description: "<p>This project was my introduction to working with APIs, with a little bit of image swapping flair on the side</p>" + 
+                        "<p>Comes preloaded with some videogame themed buttons, but anything typed into the search box becomes a loader button.</p>",
+        github: "https://github.com/davidjemerson/giftastic",
+        live: "https://davidjemerson.github.io/giftastic"
+    },
+    {
+        title: "Doctor Who Trivia",
+        subtitle: "what better way to play with timers",
+        image: "./assets/images/dwt.png",
+        description: "<p>C'mon. Is it always Doctor Who or Star Wars with this guy?</p>" + 
+                        "<p>Yes.</p>" +
+                        "<p>A pretty simple timed trivia game with a Doctor Who theme. Maybe I should add a Star Wars variant...</p>",
+        github: "https://github.com/davidjemerson/TriviaGame",
+        live: "https://davidjemerson.github.io/TriviaGame"
+    },
+    {
+        title: "Crystals Collector",
+        subtitle: "strategic random counting",
+        image: "./assets/images/crystals.png",
+        description: "<p>Crystals Collector was my first project that utilized jQuery.</p>" + 
+                        "<p>The goal of the game is to hit the target score without going over. Crystal values are hidden and reassigned with each new game.</p>",
+        github: "https://github.com/davidjemerson/week-4-game",
+        live: "https://davidjemerson.github.io/week-4-game"
+    },
+    {
         title: "Star Wars Hangman",
+        subtitle: "where it all started",
         image: "./assets/images/swh.png",
         description: "<p>It's hangman without a hangman, and all the words are proper nouns from the Star Wars universe.</p>" + 
                         "<p>My first Javascript app. Included here only for nostalgia, and to allow others to snicker at the layout.</p>",
@@ -40,9 +89,10 @@ var project = [
 function getProjects() {
     var projectContainer = $("#projects");
     for (var i = 0 ; i < project.length ; i++) {
-        var projectBlock = "<div id='" + i + "' class='mx-auto col-xl-4 col-lg-6 col-md-12 p-3 project'>" +
-        "<div class='row'><h3 class='mx-auto'>" + project[i].title + "</h3></div><img class='img-fluid' src='" + project[i].image +
-        "' alt='" + project[i].title + "' /><div class='row mt-3'><button class='mx-auto btn btn-dark'>Find Out More</button></div></div>";
+        var projectBlock = "<div id='" + i + "' class='mx-auto col-xl-4 col-lg-6 col-md-12 project'>" +
+        "<img class='img-fluid project-img' src='" + project[i].image +
+        "' alt='" + project[i].title + "' /><div class='proj-caption'><h3>" + project[i].title + "</h3>" +
+        "<p>" + project[i].subtitle + "</p></div></div>";
         projectContainer.append(projectBlock);
     }
 }
@@ -57,7 +107,6 @@ $(".project").on('click', function(event) {
     for (var j = 0 ; j < project[modalId].description.length ; j++) {
         description += "<p>" + project[modalId].description[j] + "</p>";
     }
-    console.log(description);
     $(".modal-title").text(project[modalId].title);
     $("#imgLink").attr("href", project[modalId].live);
     $("#modalImg").attr("src", project[modalId].image);
@@ -65,4 +114,34 @@ $(".project").on('click', function(event) {
     $("#githubButton").attr("href", project[modalId].github);
     $("#liveButton").attr("href", project[modalId].live);
     $('.modal-template').modal();
+});
+
+
+
+$(function(o) {
+    "use strict";
+    o('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+            var a = o(this.hash);
+            if ((a = a.length ? a : o("[name=" + this.hash.slice(1) + "]")).length)
+                return o("html, body").animate({
+                    scrollTop: a.offset().top - 50
+                }, 1e3, "easeInOutExpo"),
+                !1
+        }
+    }),
+    o(".js-scroll-trigger").click(function() {
+        o(".navbar-collapse").collapse("hide")
+    }),
+    o("body").scrollspy({
+        target: "#mainNav",
+        offset: 50
+    });
+    var a = function() {
+        50 < o("#mainNav").offset().top ? o("#mainNav").addClass("navbar-shrink") : o("#mainNav").removeClass("navbar-shrink"),
+        50 < o("#mainNav").offset().top ? o("#skills").addClass("skills-show") : o("#skills").removeClass("skills-show"),
+        50 < o("#mainNav").offset().top ? o("#portfolio").addClass("portfolio-show") : o("#portfolio").removeClass("portfolio-show")
+    };
+    a(),
+    o(window).scroll(a)
 });
